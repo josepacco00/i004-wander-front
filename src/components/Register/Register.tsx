e3a2e27411771f31a6a54a8ef24c412d23dd45cb
javierhidalgodev <javierhidalgodev@gmail.com>
2024-11-15 16:11:15 +0100

    Adaptación al diseño componente Register

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { SignUpSchema, signUpSchema } from "../../schemas/signUp.schema"
+import "./Register.css"
+import { useState } from "react"
 
 const Register: React.FC = () => {
     const {
@@ -9,56 +11,97 @@ const Register: React.FC = () => {
         reset,
         formState: { errors, isSubmitting },
     } = useForm<SignUpSchema>({
-        resolver: zodResolver(signUpSchema)
+        resolver: zodResolver(signUpSchema),
+        // defaultValues: {
+        //     name: "Javier",
+        //     email: "javier@hidalgo.com",
+        //     password: "Asdf2024!",
+        //     confirmPassword: "Asdf2024!",
+        //     role: "provider",
+        //     terms: false
+        // }
     })
+
+    // Pruebas para notificaciones
+    const [reqError, setReqError] = useState<string | null>()
+    const [successNotification, setSuccessNotification] = useState<string | null>(null)
 
     console.log(errors)
 
     const onSubmit = async (data: SignUpSchema) => {
-        const newUser = JSON.stringify(data)
-        console.log(newUser)
-        try {
-            // const response = await fetch(`${API_URL}/auth/register`, {
-            const response = await fetch(`https://reqres.in/api/users`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: newUser
-            })
 
-            const result = await response.json()
+        // Pruebas de funcionamiento de la petición
+        // const newUser = JSON.stringify(data)
+        // console.log(newUser)
 
-            console.log(result)
-        } catch (error) {
-            console.log(error)
-        }
+        // const userExample = JSON.stringify({
+        //     email: "eve.hol@reqres.in",
+        //     password: "string"
+        // })
+        // console.log(userExample)
+
+        // try {
+        //     // const response = await fetch(`${API_URL}/auth/register`, {
+        //     const response = await fetch(`https://reqres.in/api/register`, {
+        //         method: "POST",
+        //         headers: {
+        //             "Content-Type": "application/json"
+        //         },
+        //         body: userExample
+        //     })
+
+        //     const result = await response.json()
+
+        //     if (response.status === 400) {
+        //         throw Error(result.error)
+        //     } else if (response.status === 200) {
+        //         reset()
+        //         setSuccessNotification("User registered successfully")
+        //         setTimeout(() => setSuccessNotification(null), 5000)
+        //     }
+
+
+
+        // } catch (error) {
+        //     if (error instanceof Error) {
+        //         setReqError(error.message)
+        //     } else if (error && typeof error === "object" && "error" in error) {
+        //         setReqError(error.error as string)
+        //     }
+        //     setTimeout(() => setReqError(null), 5000)
+        // }
 
         // BDD request
         // await new Promise((resolve) => setTimeout(resolve, 1000))
-
-        reset()
     }
 
     return (
-        <div className="min-w-[290px] w-fit flex flex-col gap-4 px-12 py-8 bg-slate-100 border border-slate-200/50 rounded-md">
-            <p className="text-xl font-bold">Create your free account</p>
+        <div className="w-full h-dvh flex flex-col gap-4 px-12 py-8 rounded-md">
+            <div className="brand">
+                <p className="font-bold text-sm text-center">Explora nuevas aventuras</p>
+            </div>
             <form
                 onSubmit={handleSubmit(onSubmit)}
-                className="flex flex-col gap-3 [&>div]:flex [&>div]:flex-col [&>div]:gap-1 [&_label]:text-sm [&_input]:px-2 [&_input]:py-1 [&_select]:px-2 [&_select]:py-1 [&_input]:ring-1 [&_input]:ring-slate-200 [&_input]:rounded-md [&_select]:rounded-md focus:[&_input]:bg-slate-100 focus:[&_input]:outline-none focus:[&_input]:ring-2 focus:[&_input]:ring-slate-300"
+                className="flex flex-col gap-3 [&>div]:flex [&>div]:flex-col [&>div]:gap-2 [&_label]:text-sm [&_label]:text-slate-800 [&_input]:px-4 [&_input]:py-1 [&_input]:text-sm [&_input]:ring-1 [&_input]:ring-slate-200 [&_input]:rounded-full focus:[&_input]:bg-slate-100 focus:[&_input]:outline-none focus:[&_input]:ring-2 focus:[&_input]:ring-slate-300 [&_input]:cursor-default"
             >
                 <div>
                     <label htmlFor="name">Name</label>
                     <input
+                        {...register("name")}
                         type="text"
                         id="name"
-                        {...register("name")}
+                        placeholder="John Doe"
                     />
                     {errors.name && <span className="form__error-notification"> {errors.name.message} </span>}
                 </div>
                 <div>
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email" {...register("email")} />
+                    <input
+                        {...register("email")}
+                        type="email"
+                        id="email"
+                        placeholder="john@doe.com"
+                    />
                     {errors.email && <span className="form__error-notification">{errors.email.message}</span>}
                 </div>
                 <div>
@@ -86,12 +129,15 @@ const Register: React.FC = () => {
                     </select>
                     { errors.location && <span className="form__error-notification">Location is required</span> }
                     </div> */}
-                <div>
+                <div className="relative">
                     <label htmlFor="role">Role</label>
-                    <select {...register("role")} id="role">
+                    <select className="px-4 py-2 rounded-full appearance-none" id="role" {...register("role")}>
                         <option value="tourist">Tourist</option>
                         <option value="provider">Provider</option>
                     </select>
+                    <div className="absolute right-4 top-[calc(50%+4px)] p-1.5 pt-[5px] pb-[7px] rounded-full hover:bg-slate-400">
+                        <i className=" w-2 h-2 block border-slate-800 border-l-2 border-b-2 -rotate-45"></i>
+                    </div>
                     {errors.role && <span className="form__error-notification">{errors.role.message}</span>}
                 </div>
                 {/* <div>
@@ -108,10 +154,16 @@ const Register: React.FC = () => {
                     <label htmlFor="terms" className="!text-xs">Accept terms and conditions</label>
                     {errors.role && <span className="form__error-notification">{errors.role.message}</span>}
                 </div>
-                <button disabled={isSubmitting} type="submit" className="p-2 font-semibold bg-amber-400 hover:bg-amber-500 rounded-md disabled:bg-slate-400 text-center">
+                <button disabled={isSubmitting} type="submit" className="p-2 font-semibold bg-primary hover:bg-secondary rounded-full shadow-lg disabled:bg-slate-400 text-center">
                     {isSubmitting ? "Registering..." : "Register"}
                 </button>
+                <p className="text-xs text-center">
+                    ¿Ya tienes una cuenta?
+                    <a href="" className="text-primary hover:text-secondary font-bold"> Inicia sesión</a>
+                </p>
             </form>
+            {reqError && <p className="form__error-notification">{reqError}</p>}
+            {successNotification && <p className="form__success-notification">{successNotification}</p>}
         </div>
     )
 }
