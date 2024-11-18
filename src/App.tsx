fd4f78d9276d3acd5cbfc2dd6e0e926638182871
Gabriel <aviles.gabriel015@gmail.com>
2024-11-18 17:14:22 -0400

    feat: add global navegation bar (design)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css'
-import AppRoutes from './routes/AppRoutes'
+import "./App.css";
+import NavBarLayout from "./layout/NavBarLayout";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-
   return (
     <>
+      <NavBarLayout />
       <AppRoutes />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
