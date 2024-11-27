e52034e2f7c3421dbf1c274ef09d3c8df1fff586
javierhidalgodev <javierhidalgodev@gmail.com>
2024-11-27 08:21:08 +0100

    Mock para listado de teléfonos/país

---
+++ src/mocks/countriesPhoneList.mock.ts
@@ -0,0 +1,77 @@
+export const countriesPhoneList = [
+    {
+        "nombre": "Estados Unidos",
+        "prefijo": "+1",
+        "bandera": "🇺🇸"
+    },
+    {
+        "nombre": "España",
+        "prefijo": "+34",
+        "bandera": "🇪🇸"
+    },
+    {
+        "nombre": "México",
+        "prefijo": "+52",
+        "bandera": "🇲🇽"
+    },
+    {
+        "nombre": "Argentina",
+        "prefijo": "+54",
+        "bandera": "🇦🇷"
+    },
+    {
+        "nombre": "Japón",
+        "prefijo": "+81",
+        "bandera": "🇯🇵"
+    },
+    {
+        "nombre": "Reino Unido",
+        "prefijo": "+44",
+        "bandera": "🇬🇧"
+    },
+    {
+        "nombre": "Alemania",
+        "prefijo": "+49",
+        "bandera": "🇩🇪"
+    },
+    {
+        "nombre": "Francia",
+        "prefijo": "+33",
+        "bandera": "🇫🇷"
+    },
+    {
+        "nombre": "India",
+        "prefijo": "+91",
+        "bandera": "🇮🇳"
+    },
+    {
+        "nombre": "Brasil",
+        "prefijo": "+55",
+        "bandera": "🇧🇷"
+    },
+    {
+        "nombre": "China",
+        "prefijo": "+86",
+        "bandera": "🇨🇳"
+    },
+    {
+        "nombre": "Canadá",
+        "prefijo": "+1",
+        "bandera": "🇨🇦"
+    },
+    {
+        "nombre": "Italia",
+        "prefijo": "+39",
+        "bandera": "🇮🇹"
+    },
+    {
+        "nombre": "Sudáfrica",
+        "prefijo": "+27",
+        "bandera": "🇿🇦"
+    },
+    {
+        "nombre": "Australia",
+        "prefijo": "+61",
+        "bandera": "🇦🇺"
+    }
+]
