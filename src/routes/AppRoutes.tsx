b24880a11982d6b6af36c4a83d8f0246df88329e
Cristian Ayala Sánchez <ayalasanchezcristian2000@gmail.com>
2024-12-01 18:13:04 +0100

    Update AppRoutes.tsx

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom'
-// import PrivateRoute from './PrivateRoutes'
 import Register from '../components/Register/Register'
 import UserProfile from '../components/UserProfile/UserProfile'
 import Login from '../components/LoginForm/LoginForm'
@@ -22,45 +21,38 @@ const AppRoutes = () => {
 
     return (
         <Routes>
-            <Route path="/" element={<Home />} />q
+            {/* Rutas públicas */}
+            <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/user-profile" element={<UserProfile />} />
             <Route path="/login" element={<Login />} />
             <Route path="/confirm-register" element={<ConfirmRegister />} />
             <Route path="/forgot-password-verify-code" element={<ForgotPasswordVerifyCode />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/change-password" element={<ChangePassword />} />  {/* this route is not yet implemented */}
-            <Route path="/add-experience" element={<AddExperience />} />
-            <Route path="/confirmation-view" element={<ConfirmationView />} />
+            <Route path="/change-password" element={<ChangePassword />} />
             <Route path="/filters" element={<Filters />} />
-            <Route path="/experience/:id" element={<ExperienceDetail />} />
 
+            {/* Rutas protegidas */}
             <Route element={<ProtectedPublicRoute />}>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/confirm-register" element={<ConfirmRegister />} />
-                <Route path="/forgot-password-verify-code" element={<ForgotPasswordVerifyCode />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/change-password" element={<ChangePassword />} />  {/* this route is not yet implemented */}
-                <Route path="/confirmation-view" element={<ConfirmationView />} />
-            </Route>
-
-            {/* <Route element={<PrivateRoute />}> */}
                 <Route path="/user-profile" element={<UserProfile />} />
                 <Route path="/add-experience" element={<AddExperience />} />
+                <Route path="/confirmation-view" element={<ConfirmationView />} />
+                <Route path="/experience/:id" element={<ExperienceDetail />} />
+            </Route>
 
-                <Route element={<ReservationProviderWrapper />}>
-                    <Route path="/booking" element={<Book />} />
-                    <Route path="/payment-method" element={<PaymentMethod />} />
-                    <Route path="/payment-details" element={<PaymentDetails />} />
-                </Route>
+            {/* Rutas privadas */}
+            {/* Si tienes un componente como PrivateRoute, descomenta y usa el código a continuación */}
+            {/* <Route element={<PrivateRoute />}> */}
+            <Route element={<ReservationProviderWrapper />}>
+                <Route path="/booking" element={<Book />} />
+                <Route path="/payment-method" element={<PaymentMethod />} />
+                <Route path="/payment-details" element={<PaymentDetails />} />
+            </Route>
             {/* </Route> */}
 
+            {/* Ruta de 404 */}
             <Route path="*" element={<h1>404</h1>} />
-
         </Routes>
     )
 }
 
-
-export default AppRoutes
+export default AppRoutes;
