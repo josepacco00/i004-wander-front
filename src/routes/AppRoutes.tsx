a29a72b00a1a0edad6b3a332c223ed5c8c001b9c
javierhidalgodev <javierhidalgodev@gmail.com>
2024-12-03 20:52:14 +0100

    Movemos la vista de detalles de experiencia a rutas NO privadas pero que necesitan el contexto de la reserva

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-// import PrivateRoute from './PrivateRoutes'
+import PrivateRoute from './PrivateRoutes'
 import Register from '../components/Register/Register'
 import UserProfile from '../components/UserProfile/UserProfile'
 import Login from '../components/LoginForm/LoginForm'
@@ -16,7 +16,7 @@ import AddExperience from "../components/AddExperience/AddExperience";
 import Home from "../components/Home/homePage";
 import Filters from '../components/Filters/Filters'
 import ConfirmationView from '../components/ConfirmationView/ConfirmationView'
-// import { ProtectedPublicRoute } from './ProtectedPublicRoutes'
+import { ProtectedPublicRoute } from './ProtectedPublicRoutes'
 
 const AppRoutes = () => {
 
@@ -24,28 +24,35 @@ const AppRoutes = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/filters" element={<Filters />} />
-            <Route path="/experience/:id" element={<ExperienceDetail />} />
 
-            {/* <Route element={<ProtectedPublicRoute />}> */}
+            <Route element={<ProtectedPublicRoute />}>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/confirm-register" element={<ConfirmRegister />} />
                 <Route path="/forgot-password-verify-code" element={<ForgotPasswordVerifyCode />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/change-password" element={<ChangePassword />} />  {/* this route is not yet implemented */}
-            {/* </Route> */}
+            </Route>
 
-            {/* <Route element={<PrivateRoute />}> */}
+            {/* Rutas privadas sin contexto de reserva */}
+            <Route element={<PrivateRoute />}>
                 <Route path="/user-profile" element={<UserProfile />} />
                 <Route path="/add-experience" element={<AddExperience />} />
+            </Route>
 
-                <Route element={<ReservationProviderWrapper />}>
+            {/* Rutas que necesitan contexto de reserva */}
+            <Route element={<ReservationProviderWrapper />}>
+                {/* Ruta NO privada */}
+                <Route path="/experience/:id" element={<ExperienceDetail />} />
+
+                {/* Rutas PRIVADAS */}
+                <Route element={<PrivateRoute />}>
                     <Route path="/booking" element={<Book />} />
                     <Route path="/payment-method" element={<PaymentMethod />} />
                     <Route path="/payment-details" element={<PaymentDetails />} />
                     <Route path="/confirmation-view" element={<ConfirmationView />} />
                 </Route>
-            {/* </Route> */}
+            </Route>
 
             <Route path="*" element={<h1>404</h1>} />
 
