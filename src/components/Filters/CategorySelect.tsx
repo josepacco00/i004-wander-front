084c7e8ff025909d3de75302b945edf6f97e337b
Miguel <miguel.gonzalez1547@gmail.com>
2024-12-06 10:37:45 +0100

    update y detalles

--- src/components/Filters/CategorySelect.tsx
+++ src/components/Filters/CategorySelect.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import React from "react";
 
 interface CategorySelectProps {
   updateQueryParams: (params: Record<string, string | undefined>) => void;
@@ -7,21 +8,23 @@ interface CategorySelectProps {
 const CategorySelect: React.FC<CategorySelectProps> = ({ updateQueryParams }) => {
   const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedCategory = e.target.value || undefined;
-    console.log('Selected Category:', selectedCategory); // Debugging
     updateQueryParams({ category: selectedCategory });
   };
 
   return (
-    <div>
-      <label htmlFor="category" className="block text-sm font-medium text-gray-700">
+    <div className="relative">
+      <label
+        htmlFor="category"
+        className="block text-sm font-medium text-gray-700"
+      >
         Categoría
       </label>
       <select
         id="category"
         onChange={handleCategoryChange}
-        className="w-full p-2 border rounded-lg"
+        className="w-full p-3 pr-10 border rounded-full appearance-none focus:outline-none focus:ring-2 focus:ring-blue-500"
       >
-        <option value="">Todas las categorías</option>
+        <option value="">Categorías</option>
         <option value="naturaleza">Naturaleza</option>
         <option value="comida">Comida</option>
         <option value="tours">Tours</option>
@@ -29,13 +32,10 @@ const CategorySelect: React.FC<CategorySelectProps> = ({ updateQueryParams }) =>
         <option value="ciudad">Ciudad</option>
         <option value="ruralyagro">Rural y agro</option>
         <option value="eventos">Eventos</option>
-
-        
       </select>
+      <ChevronDownIcon className="absolute right-4 top-11 transform -translate-y-1/2 h-5 w-5 text-gray-400 pointer-events-none" />
     </div>
   );
 };
 
 export default CategorySelect;
-
-
